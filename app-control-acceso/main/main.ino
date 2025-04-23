ea45a2600801fd0d40ccdb6c12c20d2503f9fb57
Jose-Angel-12 <jajaraba12@gmail.com>
2025-04-23 16:04:46 -0500

    probando registro de UID con pagina wed

--- app-control-acceso/main/main.ino
+++ app-control-acceso/main/main.ino
@@ -2,7 +2,9 @@
 #include <LiquidCrystal_I2C.h>       
 #include <SPI.h>                     
 #include <MFRC522.h>                 
-#include <ESP32Servo.h>              // Librería compatible con ESP32 para controlar servos
+#include <ESP32Servo.h>             
+#include <WiFi.h>                    
+#include <WebServer.h>              
 
 // --- LCD ---
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -18,40 +20,77 @@ Servo servo;
 #define OPEN_POS 90   // Posición abierta
 #define CLOSE_POS 0   // Posición cerrada
 
-// --- UIDs permitidos ---
-String allowedUIDs[] = {
-  "43F4A914",
-  "4357FFA5"
-};
+// --- WiFi ---
+const char* ssid = "ESP32_AP";
+const char* password = "12345678";
+WebServer server(80);
 
+// --- Lista de UID permitidos ---
+#define MAX_UIDS 10
+String allowedUIDs[MAX_UIDS];
+int uidCount = 0;
+
+// --- Funciones Web ---
+void handleRoot() {
+  String html = "<html><body><h1>Control de Acceso</h1>";
+  html += "<form action='/add' method='get'>Agregar UID: <input name='uid'><input type='submit'></form>";
+  html += "<h3>UIDs Autorizados:</h3><ul>";
+  for (int i = 0; i < uidCount; i++) {
+    html += "<li>" + allowedUIDs[i] + "</li>";
+  }
+  html += "</ul></body></html>";
+  server.send(200, "text/html", html);
+}
+
+void handleAdd() {
+  if (server.hasArg("uid")) {
+    String newUID = server.arg("uid").c_str();
+    newUID.toUpperCase();
+    if (uidCount < MAX_UIDS) {
+      allowedUIDs[uidCount++] = newUID;
+      Serial.println("UID agregado: " + newUID);
+    }
+  }
+  server.sendHeader("Location", "/");
+  server.send(303);
+}
+
+// --- Setup ---
 void setup() {
   Serial.begin(115200);
 
-  // --- LCD ---
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("RFID + LCD + Servo");
-  delay(2000);
+  lcd.print("Inicializando...");
+
+  SPI.begin();
+  rfid.PCD_Init();
+
+  servo.attach(SERVO_PIN);
+  servo.write(CLOSE_POS);
+
+  // --- Iniciar WiFi como AP ---
+  WiFi.softAP(ssid, password);
+  Serial.println("AP Iniciado. IP:");
+  Serial.println(WiFi.softAPIP());
+
+  server.on("/", handleRoot);
+  server.on("/add", handleAdd);
+  server.begin();
+
   lcd.clear();
   lcd.print("Acerca tu TAG");
-
-  // --- RFID ---
-  SPI.begin();           
-  rfid.PCD_Init();       
-
-  // --- Servo ---
-  servo.attach(SERVO_PIN);    
-  servo.write(CLOSE_POS);     // Inicia cerrado
 }
 
+// --- Loop ---
 void loop() {
-  // Esperar tarjeta
+  server.handleClient();
+
   if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
     return;
   }
 
-  // Obtener UID
   String uidStr = "";
   for (byte i = 0; i < rfid.uid.size; i++) {
     if (rfid.uid.uidByte[i] < 0x10) uidStr += "0";
@@ -62,17 +101,9 @@ void loop() {
   Serial.print("UID Detectado: ");
   Serial.println(uidStr);
 
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("UID:");
-  lcd.setCursor(0, 1);
-  lcd.print(uidStr);
-  delay(2000);
-
   bool accessGranted = false;
-
-  for (String allowed : allowedUIDs) {
-    if (uidStr.equalsIgnoreCase(allowed)) {
+  for (int i = 0; i < uidCount; i++) {
+    if (uidStr.equalsIgnoreCase(allowedUIDs[i])) {
       accessGranted = true;
       break;
     }
@@ -83,7 +114,7 @@ void loop() {
     lcd.setCursor(0, 0);
     lcd.print("Acceso Permitido");
     lcd.setCursor(0, 1);
-    lcd.print("Abriendo cerradura");
+    lcd.print("Cerradura abierta");
     servo.write(OPEN_POS);
     delay(3000);
     servo.write(CLOSE_POS);
@@ -98,5 +129,5 @@ void loop() {
   lcd.clear();
   lcd.print("Acerca tu TAG");
 
-  rfid.PICC_HaltA();  // Finalizar lectura
+  rfid.PICC_HaltA();
 }
